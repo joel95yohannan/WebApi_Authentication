8260198d8ae7c6899d762fd98337218ef869ff27
Joel Yohannan <joel95yohannan@gmail.com>
2021-11-10 01:37:19 -0600

    commit code to git

---
+++ WebApiSample/WebApiSample/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebApiSample.Models;
+
+namespace WebApiSample.Controllers
+{
+    [Authorize(Roles ="Admin,Member")]
+    public class ValuesController : ApiController
+    {
+        // GET api/values
+        [HttpGet]
+        
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+        [HttpGet]
+        [Route("api/Get2")]
+        public IEnumerable<string> Get2()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPatch]
+        public IHttpActionResult DoWork(int id, [FromBody]Customer cust)
+        {
+            Customer c = new Customer();
+            c.Name = cust.Name;
+            c.ID = cust.ID;
+            c.LastName = cust.LastName;
+            return Ok(c);
+        }
+
+        // DELETE api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
